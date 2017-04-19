7bc38009b95dd2d6981d2a1bdbe99a6697cc972d
sajeetharan <sajeetharan@outlook.com>
2017-04-19 06:49:14 +0530

    updated

--- config.js
+++ config.js
@@ -2,8 +2,6 @@
 (function () {
     module.exports = {
         DB_CONFIG: {
-            CONNECTIONSTRING: 'mongodb://ticombo-user:t1combopa$$wdt1combopa$$wd@SG-ticombo-9938.servers.mongodirector.com:27017/ticombo-dev'
-            // CONNECTIONSTRING: 'mongodb://ticombo-user:t1combopa$$wd@ds149489.mlab.com:49489/ticombo-dev'
-        }
+         }
     };
 }());
